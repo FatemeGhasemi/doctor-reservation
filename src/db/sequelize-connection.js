e4b431a63f646a677930a383bdfcd68795a272ca
fateme <ghasemifateme7320@gmail.com>
2019-01-22 15:30:08 +0330

    make sequelize connection a function

--- src/db/sequelize-connection.js
+++ src/db/sequelize-connection.js
@@ -1,14 +1,17 @@
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DATABASE,null,null, {
-//     host: process.env.SEQUELIZE_HOST,
-//     dialect: process.env.SEQYELOZE_DIALECT,
-// });
-const sequelize = new Sequelize('postgres://localhost:5432/reservation');
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
+
+// const sequelize = new Sequelize('postgres://localhost:5432/reservation');
+const init = ()=> {
+    const sequelize = new Sequelize(process.env.DATABASE_NAME, null, null, {
+        host: process.env.SEQUELIZE_HOST,
+        port: process.env.POSTGRES_PORT
     });
+    sequelize
+    .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+}
