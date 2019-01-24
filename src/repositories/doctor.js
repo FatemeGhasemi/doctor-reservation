96feafe8ffe0ca96b591f4f5bb13321005113fc1
fateme <ghasemifateme7320@gmail.com>
2019-01-25 01:11:48 +0330

    Add search functions in doctor repository

--- src/repositories/doctor.js
+++ src/repositories/doctor.js
@@ -12,4 +12,16 @@ const deactivateDoctor = async (userId) => {
 //    TODO return deactivated user
 };
 
+const searchDoctorFullText = async (filter)=>{
+//    TODO return doctor list
+};
+
+const searchDoctorByCategory=(categoryId,begin=0,total=10)=>{
+//    TODO return doctor list
+}
+
+const createDoctorUser = async (phoneNumber,firstName,lastName,categoryId,description = "",officeIds=[])=>{
+//    TODO return doctor data
+}
+
 module.exports={activateAsDoctor,updateDoctorData,deactivateDoctor}
