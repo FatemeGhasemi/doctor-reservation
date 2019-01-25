0509e11466c9d5590ef71e7e6fcdd6bf8dcd7eaf
fateme <ghasemifateme7320@gmail.com>
2019-01-25 16:41:28 +0330

    Add send sms service by kaveNegar

--- src/services/sms.js
+++ src/services/sms.js
@@ -1,3 +1,22 @@
-const send = async (receptor, message) => {
-    // TODO call and return a send function that send message from special sender to receptor it has a api that kaveNegar supply it for users
+const Promise = require('bluebird');
+const kaveNegar = require('kavenegar');
+
+const kaveNegarPromisifyApi= Promise.promisifyAll(kaveNegar.KavenegarApi({
+        apikey: process.env.KAVE_NEGAR_API_KEY
+    })
+);
+const api = kaveNegarPromisifyApi;
+
+const send = async (receptor,message)=> {
+    return await api.Send({
+        message: message,
+        sender: process.env.KAVE_NEGAR_SENDER_PHONE,
+        receptor: receptor
+    });
+};
+
+
+
+module.exports ={
+    send
 };
