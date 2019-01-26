2c9a19789823fa5ad14a69908971dd161122d2df
fateme <ghasemifateme7320@gmail.com>
2019-01-26 21:23:30 +0330

    Add init-tables script

---
+++ src/init-tables.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+const category = require('./models/category');
+const office = require('./models/office');
+const reservation = require('./models/reservation');
+const doctor = require('./models/doctor');
+
+const createTables = async (sequelize) => {
+    const userSchema = require('./models/user')(sequelize);
+    const secretary = require('./models/secretary')(sequelize);
+
+
+    try {
+        await userSchema.initUserSchema().sync()
+        // await secretary.secretarySchema()
+        // await doctor.doctorSchema()
+        // await reservation.reservationSchema()
+        // await office.officeSchema()
+        // await category.categorySchema()
+    }catch (e) {
+        console.log("createTables ERROR:",e)
+    }
+}
+
+
+const init = async ()=> {
+    sequelize = await new Sequelize(process.env.DATABASE_NAME, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
+        dialect:process.env.SEQUELIZE_DIALECT,
+        host: process.env.SEQUELIZE_HOST,
+        port: process.env.POSTGRES_PORT,
+    });
+    await sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.'+process.env.DATABASE_NAME);
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+    return sequelize
+};
+
+let sequelize;
+init().then(()=>createTables(sequelize))
