6b86ff41aff11aa813b644d85018dba50d20ae46
fateme <ghasemifateme7320@gmail.com>
2019-01-27 14:10:35 +0330

    Add createNewUser function and post user successfully

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 require('dotenv').config();
-// const initializeDb = require('./db/initialize-db');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const swaggerUi = require('swagger-ui-express');
-
 const swaggerDocument = require('./swager');
+
+
 // Because production environment support ssl we should scheme of swaggerData to https to can load that in production
 if (process.env.NODE_ENVIRONMENT === 'production') {
     swaggerDocument.schemes = ['https'];
     app.use('/api-docsdjflhdjshflsjhnsjljcmslncksncscmmcasmka', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 } else app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 console.log('swaggerDocument.schemes ', swaggerDocument.schemes);
 
-// initializeDb.tablesCreator().then();
 
-
-//
-// app.use('/api/v1/login', require('./routers/v1/login-register'));
-// app.use('/api/v1/users', require('./routers/v1/user'));
-// app.use('/api/v1/login', require('./routers/v1/user'));
-// app.use('/api/v1/get-otp', require('./routers/v1/user'));
+app.use('/api/v1/users', require('./routers/v1/user'));
 
 app.listen(process.env.PORT, () => {
     console.log("Example app listening at http://%s:%s", process.env.PORT)
--- src/routers/v1/user.js
+++ src/routers/v1/user.js
@@ -1,18 +1,19 @@
 const express = require('express');
 app = express();
-const userRepository = require("../../repositories/user");
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const router = express.Router();
+userRepository = require('../../repositories/user');
 
 const createNewUser = async (req, res) => {
     try {
-        await userRepository.createUser(req.body.phoneNumber)
+        const user = await userRepository.createUser(req.body.phoneNumber)
+        res.json({"message": "success operation", "result": user})
 
     } catch (e) {
         res.status(500).json({message: e.message})
     }
 };
 
-router.post('/', createNewUser());
+router.post('/', createNewUser);
+module.exports = router;
