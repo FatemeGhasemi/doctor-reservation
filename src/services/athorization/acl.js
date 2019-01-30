cb9e1a370614c65e5460a0610c78973d1389ee0b
fateme <ghasemifateme7320@gmail.com>
2019-01-30 14:40:52 +0330

    add check roll access service by casbin

--- src/services/athorization/acl.js
+++ src/services/athorization/acl.js
@@ -1,4 +1,16 @@
-//this function show if user with special jwtToken have access to do special act on special data or object
+const casbin = require('casbin');
+const jwt = require('./jwt');
+
 const checkRoleAccess = async (jwtToken, object, act) => {
-    // TODO return true or false
+    try {
+        const enforcer = await casbin.newEnforcer('./configs/model.conf', './configs/policy.csv');
+        let phoneNumber = jwt.verifyJwt(jwtToken).phoneNumber;
+        return enforcer.enforce(phoneNumber, object, act)
+
+    } catch (e) {
+        console.log("checkRoleAccess ERROR: ", e.message)
+    }
 };
+
+
+module.exports = {checkRoleAccess};
