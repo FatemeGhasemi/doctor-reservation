328c0d3e503d1451573b315ffee8ccd4a4dc1ead
fateme <ghasemifateme7320@gmail.com>
2019-02-09 16:31:24 +0330

    write tow empty function in reserve repository

--- src/repositories/reserve.js
+++ src/repositories/reserve.js
@@ -1,6 +1,8 @@
 const reservationSchema = require('../models/reservation')();
 
 
-const creatReservation = async ()=>{
-    
-}
+const creatReservation = async (data)=>{
+
+};
+
+const counterGenerator = async (time)=>{};
